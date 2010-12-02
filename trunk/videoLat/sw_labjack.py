98295c45de72bf9a36c4d39c0b426023347160ad
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2010-12-02 22:30:31 +0000

    LabJack starts to work, for output

--- trunk/videoLat/sw_labjack.py
+++ trunk/videoLat/sw_labjack.py
@@ -1,3 +1,8 @@
+print 'file', __file__
+import os
+import sys
+dirname = os.path.dirname(__file__)
+sys.path.append(os.path.join(dirname, 'pylabjack'))
 import u3
 import time
 
@@ -16,8 +21,8 @@ class MyLabJack:
             ])
         # Store commands, for easy reference
         self.out_cmd = [
-            u3.BitStateWrite(self.OUTPUT_PORT, 0),
-            u3.BitStateWrite(self.OUTPUT_PORT, 1)
+            u3.BitStateWrite(self.OUTPUT_PORT, 1),
+            u3.BitStateWrite(self.OUTPUT_PORT, 0)
             ]
         self.in_cmd = u3.BitStateRead(self.INPUT_PORT)
          
@@ -26,11 +31,22 @@ class MyLabJack:
         
     def input(self):
         return self.d.getFeedback(self.in_cmd)[0]
-        
+
+lj = MyLabJack()
+
 def _test():
     """Output lowest bit of system time (in seconds) to the LED,
     and print the value read from the photodiode"""
-    lj = MyLabJack()
     while True:
         lj.output(int(time.time()) & 1)
         print lj.input()
+
+def newBWOutput(bool):
+    lj.output(int(bool))
+    
+def main():
+    print 'sw_labjack.main() called'
+    
+print 'sw_labjack.py executed'
+
+    
