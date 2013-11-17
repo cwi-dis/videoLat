348c3c9d93fb0a9bbd6eb5796514c10e85a558a7
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2013-11-17 21:24:02 +0000

    New project added, boilerplate

---
+++ videoLat/videoLat/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  videoLat
+//
+//  Created by Jack Jansen on 17/11/13.
+//  Copyright (c) 2013 CWI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
