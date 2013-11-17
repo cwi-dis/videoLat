20c4e448d4c9662ab0c83ea2c97195dcc292a406
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2013-11-17 21:24:02 +0000

    New project added, boilerplate

---
+++ trunk/videoLat/videoLat/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  videoLat
+//
+//  Created by Jack Jansen on 17/11/13.
+//  Copyright (c) 2013 CWI. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
