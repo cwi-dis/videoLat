b354a2cf90f2927dc1decab75bcc77ccf233c2cf
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2013-12-30 23:08:54 +0000

    First step towards dual-runmanager measurements

--- videoLat/videoLat/BaseRunManager.h
+++ videoLat/videoLat/BaseRunManager.h
@@ -15,6 +15,8 @@
 
 @interface BaseRunManager : NSObject <RunOutputManagerProtocol, RunInputManagerProtocol> {
     MeasurementType *measurementType;
+    BOOL handlesInput;
+    BOOL handlesOutput;
 }
 
 + (void)initialize;
@@ -35,5 +37,16 @@
 @property(weak) IBOutlet RunCollector *collector;
 @property(weak) IBOutlet RunStatusView *statusView;
 @property(weak) IBOutlet RunTypeView *measurementMaster;
+//
+// These properties need a bit of explanation. If the same RunManager is used for
+// both input and output the following two outlets are NOT assigned in the NIB.
+// The will then be both set to self in awakeFromNib, and this run manager handles both
+// input and output.
+// But for non-symetric measurements (say, hardware light to camera) the NIB instantiates
+// two BaseRunManager subclass instances, and ties them together through the inputCompanion
+// and outputCompanion.
+//
+@property(weak) IBOutlet BaseRunManager *inputCompanion;
+@property(weak) IBOutlet BaseRunManager *outputCompanion;
 
 @end
