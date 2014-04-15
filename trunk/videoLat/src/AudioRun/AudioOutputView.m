c6ec9df15722abdc631a25863d178ab11c4d624a
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2014-04-15 13:20:01 +0000

    Added sounds and some ui elements

--- trunk/videoLat/src/AudioRun/AudioOutputView.m
+++ trunk/videoLat/src/AudioRun/AudioOutputView.m
@@ -9,6 +9,29 @@
 #import "AudioOutputView.h"
 
 @implementation AudioOutputView
+
+- (void)awakeFromNib
+{
+    NSBundle *bundle = [NSBundle mainBundle];
+    
+    samples = [bundle pathsForResourcesOfType:@"aif" inDirectory:@"sounds"];
+    NSLog(@"Sounds: %@\n", samples);
+    [self.bSample removeAllItems];
+    NSString *filename;
+    for (filename in samples) {
+        NSArray *comps = [filename componentsSeparatedByCharactersInSet: [NSCharacterSet characterSetWithCharactersInString: @"/."]];
+        NSString *title = [comps objectAtIndex: [comps count] - 2];
+        [self.bSample addItemWithTitle:title];
+    }
+}
+- (IBAction)sampleChanged: (id) sender
+{
+    NSString *sample = [sender titleOfSelectedItem];
+    NSURL * url = [[NSURL alloc] initFileURLWithPath:
+                   [[NSBundle mainBundle] pathForResource:sample ofType:@"aif" inDirectory: @"sounds"]];
+    NSLog(@"sample URL %@\n", url);
+}
+
 - (void) showNewData
 {
 }
