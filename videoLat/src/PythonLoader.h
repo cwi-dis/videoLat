45e5e7d98c0672c4e6e768f39ab36ead09c77871
jackjansen <jackjansen@c12e0cf1-bb61-4ee9-8ffd-32dc72649763>
2014-09-23 15:20:43 +0000

    More documentation

--- videoLat/src/PythonLoader.h
+++ videoLat/src/PythonLoader.h
@@ -8,11 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+///
+/// Load Python code.
+/// Because the Python code runs in the same binary as videoLat it has access to all
+/// the objects in the program, through the pyobjc pytho<->ObjC bridge.
+/// For an example, see @see LabJackDevice which implements the @see HardwareLightProtocol
+/// and can then be instantiate in the NIB file and connected to the right objects.
+///
 @interface PythonLoader : NSObject
-+ (PythonLoader *)sharedPythonLoader;
++ (PythonLoader *)sharedPythonLoader;	//!< Singleton pattern
 
-- (PythonLoader *)init;
+- (PythonLoader *)init;	//!< Internal: initialize the loader object
+
+///
+/// Load a Python script.
+/// @param script the URL of the script to load (must be a local file)
+/// @return true if successful
+///
 
 - (BOOL)loadURL: (NSURL *)script;
+///
+/// Load a Python script from a named resource.
+/// @param name the name of the script
+/// @return true if successful
+///
 - (BOOL)loadScriptNamed: (NSString *)name;
 @end
--- videoLat/src/VideoRun/genQRcodes.h
+++ videoLat/src/VideoRun/genQRcodes.h
@@ -10,12 +10,14 @@
 #import "protocols.h"
 #include "zint.h"
 
-
+///
+/// QR code generator. Uses the zint library to create greyscale image data prepresenting a QR code.
+///
 @interface GenQRcodes : NSObject <OutputVideoGenProtocol> {
-    struct zint_symbol *symbol;
+    struct zint_symbol *symbol;	//!< Internal: reference to the zint symbol generator.
 }
 
-- (GenQRcodes*)init;
-- (void) gen: (void*)buffer width: (int)width height: (int)height code: (const char *)code;
+- (GenQRcodes*)init; //!< Initialize the object.
+- (void) gen: (void*)buffer width: (int)width height: (int)height code: (const char *)code; //!< Generate a QR code.
 
 @end
