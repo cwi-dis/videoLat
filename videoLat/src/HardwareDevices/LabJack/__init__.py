52888d2f41a4223cf9221ada821b186cef6793ae
Jack Jansen <Jack.Jansen@cwi.nl>
2018-04-03 17:03:28 +0200

    Logic for LabJack seems to be inverted... Sigh...

--- videoLat/src/HardwareDevices/LabJack/__init__.py
+++ videoLat/src/HardwareDevices/LabJack/__init__.py
@@ -9,7 +9,7 @@ from Cocoa import *
 import objc
 import threading
 
-DEBUG=False
+DEBUG=True
 
 HardwareLightProtocol = objc.protocolNamed('HardwareLightProtocol')
 
@@ -29,6 +29,7 @@ class LabJack(NSObject, HardwareLightProtocol):
         self = super(LabJack, self).init()
         self.u3Device = None
         self._lastErrorMessage = None
+        self._lastOutputState = None
         self.lock = threading.Lock()
         return self
     
@@ -45,11 +46,15 @@ class LabJack(NSObject, HardwareLightProtocol):
             self._lastErrorMessage = 'Cannot open LabJack U3: %s' % arg
             return
         # Configure all ports as digital
-        self.u3Device.configIO(0)
-        self.u3Device.getFeedback([
+        conf = self.u3Device.configIO(0)
+        if DEBUG: print 'LabJack: config=', conf
+        rv = self.u3Device.getFeedback([
             u3.BitDirWrite(self.INPUT_PORT, 0),
             u3.BitDirWrite(self.OUTPUT_PORT, 1),
+            u3.BitDirRead(self.INPUT_PORT),
+            u3.BitDirRead(self.OUTPUT_PORT)
             ])
+        if DEBUG: print 'LabJack: getFeedback returned', rv
 
     def lastErrorMessage(self):
         """Returns last error message from hardware/library, for display to the user."""
@@ -62,11 +67,11 @@ class LabJack(NSObject, HardwareLightProtocol):
         """Returns true if the library is installed and the hardware connected."""
         with self.lock:
             try:
-                if DEBUG: print 'LabJack.available: called with self=', self
-                #import pdb ; pdb.set_trace()
                 if not self.u3Device:
+                    if DEBUG: print 'LabJack.available: called with self=', self
+                    #import pdb ; pdb.set_trace()
                     self._tryOpen()
-                if DEBUG: print 'LabJack.available: u3device is', self.u3Device
+                    if DEBUG: print 'LabJack.available: u3device is', self.u3Device
                 return not not self.u3Device
             except:
                 if DEBUG: print 'LabJack.available: exception during _tryopen'
@@ -81,17 +86,20 @@ class LabJack(NSObject, HardwareLightProtocol):
         """Set output light level to 'level' and read return input light level."""
         with self.lock:
             try:
-                if DEBUG: print 'LabJack: light_ called', self, level
+                if DEBUG: print 'LabJack: light_ called', self, level,
                 if not self.u3Device:
                     return 0
                 # Note: the logic is reversed here: 0 means light
-                if level < 0.5:
-                    outCmd = u3.BitStateWrite(self.OUTPUT_PORT, 1)
-                else:
-                    outCmd = u3.BitStateWrite(self.OUTPUT_PORT, 0)
-                inCmd = u3.BitStateRead(self.INPUT_PORT)
-                rv = self.u3Device.getFeedback([inCmd, outCmd])
-                if DEBUG: print 'labJackDevice: light_: returned', rv
+                commands = [u3.BitStateRead(self.INPUT_PORT)]
+                if level != self._lastOutputState:
+                    self._lastOutputState = level
+                    commands.append(u3.BitDirWrite(self.OUTPUT_PORT, 1))
+                    if level >= 0.5:
+                        commands.append(u3.BitStateWrite(self.OUTPUT_PORT, 1))
+                    else:
+                        commands.append(u3.BitStateWrite(self.OUTPUT_PORT, 0))
+                rv = self.u3Device.getFeedback(commands)
+                if DEBUG: print 'returned', rv
                 return rv[0]
             except u3.LabJackException, arg:
                 self._lastErrorMessage = 'Exception during LabJack.light_: %s' % arg
