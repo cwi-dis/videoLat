179072b38643b094170ae2d57d9010b3c60d46cf
Jack Jansen <Jack.Jansen@cwi.nl>
2019-06-19 11:05:30 +0200

    Revert "Adding null calibration (testing only)"
    This reverts commit 03200331b028166118a55c4deb9c2919de88e032.

--- videoLat/src/CommonAppDelegate.m
+++ videoLat/src/CommonAppDelegate.m
@@ -34,7 +34,6 @@
     NSURL *url = [self directoryForCalibrations];
     if (url == nil) return;
     [self _loadCalibrationsFrom:url];
-    [self _loadNullCalibration];
     
     // Initialize run manager classes. Should be done differently.
     // But I think this also ensures the class is available at runtime
@@ -150,17 +149,6 @@
     [uuidToURL setObject: url forKey:uuid];
 	
 	return YES;
-}
-
-- (void)_loadNullCalibration
-{
-    MeasurementDataStore *dataStore = [[MeasurementDataStore alloc] init];
-    dataStore.measurementType = @"null";
-    dataStore.date = @"";
-    dataStore.description = @"Null measurement (testing only)";
-    [dataStore addDataPoint: @"" sent: 0 received: 0];
-    MeasurementType *myType = [MeasurementType forType: dataStore.measurementType];
-    [myType addMeasurement: dataStore];
 }
 
 - (BOOL)haveCalibration: (NSString *)uuid
