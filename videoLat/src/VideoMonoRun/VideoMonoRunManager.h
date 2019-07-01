734f18089615c527f55fe69f65654044a65fd117
Jack Jansen <Jack.Jansen@cwi.nl>
2019-07-01 23:53:49 +0200

    VideoMonoRunManager is now also a finder and genner and has those instance variables pointing to itself

--- videoLat/src/VideoMonoRun/VideoMonoRunManager.h
+++ videoLat/src/VideoMonoRun/VideoMonoRunManager.h
@@ -15,7 +15,7 @@
 /// Overrides a minimal number of methods to handle black/white, a lot of the other
 /// changes are made through the NIB file. Compatible with HardwareRunManager.
 ///
-@interface VideoMonoRunManager : VideoRunManager {
+@interface VideoMonoRunManager : VideoRunManager<InputVideoFindProtocol, OutputVideoGenProtocol> {
     // Black/white detection
     int minInputLevel;             //!< Internal: darkest color seen during prerun
     int maxInputLevel;             //!< Internal: lightest color seen during prerun
@@ -36,8 +36,10 @@
 
 + (void)initialize;
 - (VideoMonoRunManager *)init;
-
 - (CIImage *)newOutputStart;
-
+// InputVideoFindProtocol
+- (NSString *) find: (CVImageBufferRef)image;
+// OutputVideoGenProtocol
+- (CIImage *) genImageForCode: (NSString *)code size:(int)size;
 @end
 
