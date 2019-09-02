a66631c31373ba80be2f50d608684ccd056a21de
Jack Jansen <Jack.Jansen@cwi.nl>
2019-09-02 23:21:00 +0200

    For now ignore master results

--- videoLat/src/Networking/NetworkIODevice.m
+++ videoLat/src/Networking/NetworkIODevice.m
@@ -278,7 +278,7 @@ static uint64_t getTimestamp(NSDictionary *data, NSString *key)
             [self.protocol close];
             if (self.protocol) self.protocol.delegate = nil;
             self.protocol = nil;
-            assert(0); // xxxjack this has to go somewhere...
+            NSLog(@"xxxjack ignoring results returned by master for now....");
             // [self.manager reportResultsToRemote: mr];
             //
             // Override description with our description
