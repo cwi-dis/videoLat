8dfa3e78adf6fe2793d9b56ba134677a0c079199
Jack Jansen <Jack.Jansen@cwi.nl>
2020-02-28 14:32:07 +0100

    Get color from squares

--- videoLat/src/VideoRun/FindSquares.m
+++ videoLat/src/VideoRun/FindSquares.m
@@ -44,7 +44,7 @@
     CIRectangleFeature *feature = features[0];
     CIImage *matchedSquareImage = [self squareImageForFeature:ciImage feature:feature];
     //NSLog(@"Square image width=%f height=%f", matchedSquareImage.extent.size.width, matchedSquareImage.extent.size.height);
-    //[self dumpImage: matchedSquareImage to: @"xxxjack-outerSquare.png"];
+    [self dumpImage: matchedSquareImage to: @"xxxjack-outerSquare.png"];
     // Now Find the five colored squares inside it
     int nFound = 0;
     float subRects[5][2] = {
@@ -56,16 +56,37 @@
     };
     
     for (int i=0; i<5; i++) {
-        CIImage *mid = [self subImage: matchedSquareImage top: subRects[i][0] left: subRects[i][1]];
+        CIImage *midImage = [self subImage: matchedSquareImage top: subRects[i][0] left: subRects[i][1]];
+        [self dumpImage: midImage to: [NSString stringWithFormat:@"xxxjack-inner%d.png", i]];
+		NSLog(@"sub %d: midImage x=%f y=%f h=%f w=%f", i, midImage.extent.origin.x, midImage.extent.origin.y, midImage.extent.size.width, midImage.extent.size.height);
         // Find a square approximately in the right place
-        features = [detector featuresInImage:mid];
+        features = [detector featuresInImage:midImage];
         if (features.count == 1) {
             nFound++;
             // Average the color in the area of the inner square
             feature = features[0];
-            CIImage *subSquareImage = [self squareImageForFeature:mid feature:feature];
+            NSLog(@"sub %d: feature %@", i, feature);
+            CIImage *subSquareImage = [self squareImageForFeature:midImage feature:feature];
+			[self dumpImage: subSquareImage to: [NSString stringWithFormat:@"xxxjack-feature%d.png", i]];
             // Average the color
-            NSLog(@"To be done: average the color");
+			CIVector *subExtent = [CIVector vectorWithX:subSquareImage.extent.origin.x
+														 Y:subSquareImage.extent.origin.y
+														 Z:subSquareImage.extent.size.width
+														 W:subSquareImage.extent.size.height];
+			CIFilter *subFilter = [CIFilter filterWithName:@"CIAreaAverage"
+									keysAndValues:
+									kCIInputImageKey, subSquareImage,
+									kCIInputExtentKey, subExtent,
+									nil
+								];
+			CIImage *subPixelImage = subFilter.outputImage;
+			NSLog(@"sub %d: subSquareImage x=%f y=%f h=%f w=%f", i, subSquareImage.extent.origin.x, subSquareImage.extent.origin.y, subSquareImage.extent.size.width, subSquareImage.extent.size.height);
+			NSLog(@"sub %d: subPixelImage x=%f y=%f h=%f w=%f", i, subPixelImage.extent.origin.x, subPixelImage.extent.origin.y, subPixelImage.extent.size.width, subPixelImage.extent.size.height);
+			uint8_t subColorValues[4] = {42, 42, 42, 42};
+			CGRect bounds = CGRectMake(0, 0, 1, 1);
+			CIContext *subContext = [CIContext context];
+			[subContext render:subPixelImage toBitmap:subColorValues rowBytes:sizeof(subColorValues) bounds:bounds format:kCIFormatRGBA8 colorSpace:NULL];
+            NSLog(@"sub %d: %d %d %d %d", i, subColorValues[0], subColorValues[1], subColorValues[2], subColorValues[3]);
             // Convert to HLS or HSV or so
             // Get the primary/secondary value
         }
@@ -81,21 +102,21 @@
     if (rect.size.width == 0 || rect.size.height == 0) {
         rect = ciImage.extent;
     }
-    CIVector *ciExtent = [CIVector vectorWithX:rect.origin.x
-                                             Y:rect.origin.y
-                                             Z:rect.size.width
-                                             W:rect.size.height];
-    CIFilter *filter = [CIFilter filterWithName:@"CIAreaAverage"
-                            keysAndValues:
-                            kCIInputImageKey, ciImage,
-                            kCIInputExtentKey, ciExtent,
-                            nil
-                        ];
-    [filter setValue:ciImage forKey:kCIInputImageKey];
-    CIImage *outputImage = filter.outputImage;
-    unsigned char bytes[4];
-    CGRect bounds = CGRectMake(0, 0, 1, 1);
-    [context render:outputImage toBitmap:bytes rowBytes:4 bounds:bounds format:kCIFormatL8 colorSpace:NULL];
+			CIVector *ciExtent = [CIVector vectorWithX:rect.origin.x
+													 Y:rect.origin.y
+													 Z:rect.size.width
+													 W:rect.size.height];
+			CIFilter *filter = [CIFilter filterWithName:@"CIAreaAverage"
+									keysAndValues:
+									kCIInputImageKey, ciImage,
+									kCIInputExtentKey, ciExtent,
+									nil
+								];
+			[filter setValue:ciImage forKey:kCIInputImageKey];
+			CIImage *outputImage = filter.outputImage;
+			unsigned char bytes[4];
+			CGRect bounds = CGRectMake(0, 0, 1, 1);
+			[context render:outputImage toBitmap:bytes rowBytes:4 bounds:bounds format:kCIFormatL8 colorSpace:NULL];
     average = bytes[0];
     // Complicated way to keep black and white level but adjust to changing camera apertures
 #if 1
